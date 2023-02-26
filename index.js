9dec2c2a49ad4c655fbaaabe476d73a988e4b453
Dean <dean@dpb.pw>
2023-02-25 22:55:22 -0800

    Updated Index.js and ran npm i locally

--- index.js
+++ index.js
@@ -56,11 +56,18 @@ const questions = [
     },
 ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log('Generating README...');
+        writeToFile('README.md', generateMarkdown({ ...inquirerResponses}));
+    });
+}
 
 // Function call to initialize app
 init();
